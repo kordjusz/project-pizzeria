c7f53b9aa267344442d50cb289158c734163a4c6
Kordian Juszkiewicz <kordjusz@gmail.com>
2021-06-22 18:34:54 +0200

    Fixing bugs in accordion

--- src/js/script.js
+++ src/js/script.js
@@ -89,15 +89,15 @@
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
-        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
         console.log(activeProduct);
         
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        if(activeProduct && activeProduct != thisProduct.element){
-          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        if(activeProduct !== null && activeProduct != thisProduct.element){
+          activeProduct.classList.remove('active');
         }
         /* toggle active class on thisProduct.element */
-        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);       
+        thisProduct.element.classList.toggle('active');       
       });
     }
   }
