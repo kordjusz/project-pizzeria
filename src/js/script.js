fa29ef2c41eb2f2f810ddff95426ffe38bd809d7
Kordian Juszkiewicz <kordjusz@gmail.com>
2021-07-09 15:20:41 +0200

    Finished functionality that allows to add pictures to products while choosing ingredient

--- src/js/script.js
+++ src/js/script.js
@@ -71,13 +71,9 @@
     renderInMenu(){
       const thisProduct = this;
 
-      /* generate HTML based on a template */
       const generatedHTML = templates.menuProduct(thisProduct.data);
-      /* create element using utilis.createElementFromHTML */
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-      /* find menu container */
       const menuContainer = document.querySelector(select.containerOf.menu);
-      /* add element to menu*/
       menuContainer.appendChild(thisProduct.element);
     }
     getElements(){
@@ -87,26 +83,22 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
     initAccordion(){
       const thisProduct = this;
   
-      /* find the clickable trigger (the element that should react to clicking) */
-      // const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-  
-      /* START: add event listener to clickable trigger on event click */
+      
       thisProduct.accordionTrigger.addEventListener('click', function(event) {
-        /* prevent default action for event */
         event.preventDefault();
-        /* find active product (product that has active class) */
+       
         const activeProduct = document.querySelector(select.all.menuProductsActive);
         console.log(activeProduct);
         
-        /* if there is active product and it's not thisProduct.element, remove class active from it */
         if(activeProduct !== null && activeProduct != thisProduct.element){
           activeProduct.classList.remove('active');
         }
-        /* toggle active class on thisProduct.element */
+        
         thisProduct.element.classList.toggle('active');       
       });
     }
@@ -131,26 +123,27 @@
     }
     processOrder() {
       const thisProduct = this;
-    
-      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
       console.log('formData', formData);
-    
-      // set price to default price
       let price = thisProduct.data.price;
     
-      // for every category (param)...
       for(let paramId in thisProduct.data.params) {
-        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
         console.log(paramId, param);
     
-        // for every option in this category
         for(let optionId in param.options) {
-          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+
           const option = param.options[optionId];
+          const optionImage = thisProduct.imageWrapper.querySelector(`.${paramId}-${optionId}`);
+          console.log(optionImage);
           console.log(optionId, option); 
+
           if(formData[paramId] && formData[paramId].includes(optionId)){
+            
+            if(optionImage){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            }
+            
             if(option.price !== true){
               price += option.price;
             } else {
@@ -158,11 +151,13 @@
                 price -= option.price;
               }
             }
+          } else {
+            if(optionImage){
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
           }
         }
       }
-    
-      // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
   }
